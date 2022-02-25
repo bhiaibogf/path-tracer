1352489694162471a48b9fd58601ee72b79cd8d9
bhiaibogf <bhiaibogf@outlook.com>
2022-02-25 22:57:02 +0800

    refactor Bound

--- src/bvh/bound.cpp
+++ src/bvh/bound.cpp
@@ -5,14 +5,14 @@
 #include "bound.h"
 
 Bound::Bound() {
-    float min_float = std::numeric_limits<float>::lowest();
-    float max_float = std::numeric_limits<float>::max();
-    min_ = global::Vector(max_float, max_float, max_float);
-    max_ = global::Vector(min_float, min_float, min_float);
+    min_ = global::Vector(kMaxFloat, kMaxFloat, kMaxFloat);
+    max_ = global::Vector(kMinFloat, kMinFloat, kMinFloat);
 }
 
+Bound::Bound(const global::Vector &min, const global::Vector &max) : min_(min), max_(max) {}
+
 bool Bound::Intersect(const Ray &ray) const {
-    float t_enter = -std::numeric_limits<float>::infinity(), t_exit = std::numeric_limits<float>::infinity();
+    float t_enter = kMinFloat, t_exit = kMaxFloat;
 
     for (int i = 0; i < 3; i++) {
         float t_min = (min_[i] - ray.origin()[i]) / ray.direction()[i],
@@ -24,4 +24,51 @@ bool Bound::Intersect(const Ray &ray) const {
     }
 
     return t_enter <= t_exit && t_enter < ray.t_max() && t_exit >= ray.t_min();
+}
+
+int Bound::MaxExtent() const {
+    global::Vector d = Diagonal();
+    if (d.x() > d.y() && d.x() > d.z()) {
+        return 0;
+    } else if (d.y() > d.z()) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+global::Vector Bound::Centroid() {
+    return 0.5 * min_ + 0.5 * max_;
+}
+
+Bound Bound::operator|(const Bound &other) const {
+    return {Min(min_, other.min_), Max(max_, other.max_)};
+}
+
+Bound Bound::operator|=(const Bound &other) {
+    min_ = Min(min_, other.min_);
+    max_ = Max(max_, other.max_);
+    return *this;
+}
+
+Bound Bound::operator+(const global::Vector &other) const {
+    return {Min(min_, other), Max(max_, other)};
+}
+
+Bound Bound::operator+=(const global::Vector &other) {
+    min_ = Min(min_, other);
+    max_ = Max(max_, other);
+    return *this;
+}
+
+global::Vector Bound::Diagonal() const {
+    return max_ - min_;
+}
+
+global::Vector Bound::Min(const global::Vector &a, const global::Vector &b) {
+    return {std::min(a.x(), b.x()), std::min(a.y(), b.y()), std::min(a.z(), b.z())};
+}
+
+global::Vector Bound::Max(const global::Vector &a, const global::Vector &b) {
+    return {std::max(a.x(), b.x()), std::max(a.y(), b.y()), std::max(a.z(), b.z())};
 }
--- src/bvh/bound.h
+++ src/bvh/bound.h
@@ -13,57 +13,35 @@ class Bound {
 public:
     Bound();
 
-    Bound(global::Vector min, global::Vector max) : min_(min), max_(max) {}
+    Bound(const global::Vector &min, const global::Vector &max);
 
     ~Bound() = default;
 
     bool Intersect(const Ray &ray) const;
 
-    int MaxExtent() const {
-        global::Vector d = Diagonal();
-        if (d.x() > d.y() && d.x() > d.z())
-            return 0;
-        else if (d.y() > d.z())
-            return 1;
-        else
-            return 2;
-    }
+    int MaxExtent() const;
 
-    global::Vector Centroid() { return 0.5 * min_ + 0.5 * max_; }
+    global::Vector Centroid();
 
-    Bound operator|(const Bound &other) const {
-        return {Min(min_, other.min_), Max(max_, other.max_)};
-    }
+    Bound operator|(const Bound &other) const;
 
-    Bound operator+(const global::Vector &other) const {
-        return {Min(min_, other), Max(max_, other)};
-    }
+    Bound operator|=(const Bound &other);
 
-    Bound operator|=(const Bound &other) {
-        min_ = Min(min_, other.min_);
-        max_ = Max(max_, other.max_);
-        return *this;
-    }
+    Bound operator+(const global::Vector &other) const;
 
-    Bound operator+=(const global::Vector &other) {
-        min_ = Min(min_, other);
-        max_ = Max(max_, other);
-        return *this;
-    }
+    Bound operator+=(const global::Vector &other);
 
 private:
+    const float kMinFloat = std::numeric_limits<float>::lowest();
+    const float kMaxFloat = std::numeric_limits<float>::max();
+
     global::Vector min_, max_;
 
-    global::Vector Diagonal() const { return max_ - min_; }
+    global::Vector Diagonal() const;
 
-    global::Vector Min(const global::Vector &a, const global::Vector &b) const {
-        return global::Vector(std::min(a.x(), b.x()), std::min(a.y(), b.y()), std::min(a.z(), b.z()));
-    }
+    static global::Vector Min(const global::Vector &a, const global::Vector &b);
 
-    global::Vector Max(const global::Vector &a, const global::Vector &b) const {
-        return global::Vector(std::max(a.x(), b.x()), std::max(a.y(), b.y()), std::max(a.z(), b.z()));
-    }
-
+    static global::Vector Max(const global::Vector &a, const global::Vector &b);
 
 };
 
