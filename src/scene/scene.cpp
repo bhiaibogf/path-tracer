f1fa9312cbe42db6bff7634986619bb6a1536dc6
bhiaibogf <bhiaibogf@outlook.com>
2022-03-17 19:24:57 +0800

    update png

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -7,7 +7,7 @@
 const Eigen::Vector3f Scene::kBackgroundColor = global::Color(0.2f, 0.4f, 0.8f);
 const float Scene::kEpsilonPosition = 1e-5f;
 const float Scene::kEpsilonLight = 10.f * kEpsilonPosition;
-const float Scene::kEpsilonPdf = 1e-2f;
+const float Scene::kEpsilonPdf = 1e-6f;
 const float Scene::kRussianRoulette = 0.9f;
 const int Scene::kMaxBounce = 10;
 
