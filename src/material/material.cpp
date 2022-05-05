b15e8d47b53c92cc00cf66ea16b6f4364df686be
bhiaibogf <bhiaibogf@outlook.com>
2022-05-05 14:27:07 +0800

    [hack]remove omp critical

--- src/material/material.cpp
+++ src/material/material.cpp
@@ -15,46 +15,26 @@ bool Material::HasEmitter() const {
 void Material::Prepare(const global::TexCoord &tex_coord) {}
 
 global::Color Material::Albedo(const global::TexCoord &tex_coord) {
-    global::Color albedo;
-#pragma omp critical
-    {
-        Prepare(tex_coord);
-        albedo = Albedo();
-    }
-    return albedo;
+    Prepare(tex_coord);
+    return Albedo();
 }
 
 global::Color Material::Eval(const global::Vector &wo, const global::Vector &wi, const global::Vector &normal,
                              const global::TexCoord &tex_coord) {
-    global::Color color;
-#pragma omp critical
-    {
-        Prepare(tex_coord);
-        color = Eval(wo, wi, normal);
-    }
-    return color;
+    Prepare(tex_coord);
+    return Eval(wo, wi, normal);
 }
 
 global::Vector Material::Sample(const global::Vector &wo, const global::Vector &normal, float *pdf,
                                 const global::TexCoord &tex_coord) {
-    global::Color wi;
-#pragma omp critical
-    {
-        Prepare(tex_coord);
-        wi = Sample(wo, normal, pdf);
-    }
-    return wi;
+    Prepare(tex_coord);
+    return Sample(wo, normal, pdf);
 }
 
 float Material::Pdf(const global::Vector &wo, const global::Vector &wi, const global::Vector &normal,
                     const global::TexCoord &tex_coord) {
-    float pdf;
-#pragma omp critical
-    {
-        Prepare(tex_coord);
-        pdf = Pdf(wo, wi, normal);
-    }
-    return pdf;
+    Prepare(tex_coord);
+    return Pdf(wo, wi, normal);
 }
 
 std::ostream &operator<<(std::ostream &os, const Material &material) {
